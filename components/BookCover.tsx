ee74bea8ff00e2c31847f6aea822b751c21847f4
armand benjamin <armandbenjamin30@gmail.com>
2025-06-08 11:16:36 +0200

    refactor: update BookCover component for improved props handling and add image support in next.config

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,4 +1,8 @@
+import { cn } from '@/lib/utils';
 import React from 'react'
+import Image from 'next/image';
+
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular"  | "wide";
 
 const variantStyles: Record<BookCoverVariant, string> = {
     extraSmall: "book-cover_extra_small",
@@ -10,14 +14,20 @@ const variantStyles: Record<BookCoverVariant, string> = {
 
 interface Props {
     className?: string;
-    variant: "wide" | "small";
+    variant?: BookCoverVariant;
     coverColor: string;
-    coverUrl: string;
+    coverImage: string;
 }
 
-const BookCover = ({className, variant, coverUrl, coverColor}: Props) => {
+const BookCover = ({className, variant = 'regular', coverImage = 'https://placeholder.co/400x600.png', coverColor = '#012B48'}: Props) => {
   return (
-    <div>BookCover</div>
+    <div className={cn('relative transition-all duration-300', variantStyles[variant], className)}>
+      Book Side svg
+
+      <div className='absolute z-10' style={{left: '120%', width: '87.5%', height: '88%'}} >
+        <Image src={coverImage} alt={'Book cover'} fill className='rounded-sm object-fill' />
+      </div>
+    </div>
   )
 }
 
